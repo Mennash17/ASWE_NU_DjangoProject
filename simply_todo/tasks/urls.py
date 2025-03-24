facc31516950b2597854bd79c0e700d607748bd8
Mennash17 <mennatallahsh17@gmail.com>
2025-03-24 18:41:54 +0200

    style modification

--- simply_todo/tasks/urls.py
+++ simply_todo/tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('update/<int:pk>/', views.task_update, name='task_update'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
+
 ]
