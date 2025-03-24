50af3d2cf3978eb75c8f231043f67891c0315854
Mennash17 <mennatallahsh17@gmail.com>
2025-03-24 23:25:17 +0200

    users validations

--- simply_todo/simply_todo/settings.py
+++ simply_todo/simply_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s5$=t)$&y_n4+x!n_sz-qjtuls2q@&jn-ie6)kp$o^v7=)6d7b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simply_todo.onrender.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
